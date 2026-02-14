8b5a5fa9ce390ecf11cc485c5a7f56a7f2bcabfa
Ryma Djoudad <zenyod85@gmail.com>
2026-02-14 13:30:34 -0500

    Created variables and printed instructions

---
+++ Lab5_rdjoudad-2.py
@@ -0,0 +1,7 @@
+"""
+Pick Up Sticks Game In Python
+Ryma Djoudad
+Start with 13 sticks, players alternate trying to remove sticks, player who picks up the last stick wins
+No starter code 
+2/14/2026
+"""
